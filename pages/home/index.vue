bd34a51e9c3595224ad112142f3a17e2cb5e6b59
苏震 <suzhen@viewhigh.com>
2020-08-19 10:15:38 +0800

    title

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
     <div class="banner">
       <div class="container">
         <!-- <h1 class="logo-font">conduit</h1> -->
-        <h1 class="logo-font">1989-荣光无限</h1>
+        <h1 class="logo-font">1989-荣光无限-Red</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
